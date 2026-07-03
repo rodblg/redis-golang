2c89cfadc685ad9e3c65bd9106d3f7db3a059d4c
Rodrigo Blancas <rodrigoblancas19@gmail.com>
2026-07-02 21:30:02 -0600

    feat: add RespArray function to handle RESP array bulk strings

--- server/tcpS.go
+++ server/tcpS.go
@@ -75,13 +75,13 @@ func handleConnection(conn net.Conn) {
 // Write a function that reads $\r\n\r\n and returns the string. Read the length first, then read exactly that many bytes.
 // Test with a hardcoded byte slice before wiring it to the socket
 
-type BulkString struct {
+type RespBulkString struct {
 	Length  int
 	Message string
 }
 
 // a bulk string represent a single binary string. RESP encodes as $<length>\r\n<data>\r\n
-func ParseBulkString(input []byte) (*BulkString, error) {
+func ParseBulkString(input []byte) (*RespBulkString, error) {
 
 	reader := bufio.NewReader(bytes.NewReader(input))
 
@@ -105,11 +105,76 @@ func ParseBulkString(input []byte) (*BulkString, error) {
 		return nil, err
 	}
 
-	return &BulkString{Length: length, Message: string(msgBuff)}, nil
+	return &RespBulkString{Length: length, Message: string(msgBuff)}, nil
 	//Return BulkString
 }
 
+type RespArray struct {
+	Length   int
+	Elements []any
+}
+
+// TODO Refactor ParseArray to rm duplicated code for bulk strings
 // Clients send commands to redis as RESP arrays
 // *<number-of-elements>\r\n<element-1>...<element-n>
 // EXAMPLE *2\r\n$4\r\nLLEN\r\n$6\r\nmylist\r\n
-func parseArray() {}
+func ParseArray(input []byte) (*RespArray, int, error) {
+
+	buffer := bufio.NewReader(bytes.NewReader(input))
+
+	//Find number of elements+\r\n
+	line, err := buffer.ReadString('\n')
+	if err != nil {
+		return nil, 0, err
+	}
+	slog.Info(line)
+
+	line = strings.TrimPrefix(line, "*")
+	line = strings.TrimSuffix(line, "\r\n")
+
+	nElements, err := strconv.Atoi(line)
+	if err != nil {
+		return nil, 0, err
+	}
+	//If its not zero we continue
+	if nElements == 0 {
+		return &RespArray{}, 0, nil
+	}
+
+	var elements []any
+	for {
+		elementLine, err := buffer.ReadString('\n')
+		if err != nil {
+			slog.Error("error#buffer", "error", err)
+			break
+		}
+		slog.Info("element#line", "line", elementLine)
+		//clean array element
+		//find which element is
+		elementLine = strings.TrimSuffix(elementLine, "\r\n")
+		elementLine = strings.TrimPrefix(elementLine, "$")
+
+		length, err := strconv.Atoi(elementLine)
+		if err != nil {
+			slog.Error("error#Atoi", "error", err)
+			return &RespArray{}, 0, nil
+		}
+		slog.Info("element#length", "length", length)
+		//we increase the length by 2 so we scan the full msg + the \r\n that separates from the next bulk
+		msgBuff := make([]byte, length+2)
+		_, err = io.ReadFull(buffer, msgBuff)
+		if err != nil {
+			slog.Error("error#ReadBuff", "error", err)
+			return &RespArray{}, 0, nil
+		}
+		strMsgBuff := strings.TrimSuffix(string(msgBuff), "\r\n")
+		slog.Info("element#msg", "element", strMsgBuff)
+		elements = append(elements, strMsgBuff)
+	}
+
+	return &RespArray{
+		Length:   nElements,
+		Elements: elements,
+	}, nElements, nil
+
+}
