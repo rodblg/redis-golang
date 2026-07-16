fd7f5828f8d32be8674e7f6a09078fb2a45e1dfc
Rodrigo Blancas <rodrigoblancas19@gmail.com>
2026-07-15 20:08:16 -0600

    test: add happy path for ECHO command

--- server/tcpS_test.go
+++ server/tcpS_test.go
@@ -21,3 +21,17 @@ func TestHandleRespInput(t *testing.T) {
 	}
 
 }
+
+func TestHandleRespEcho(t *testing.T) {
+	reader := bufio.NewReader(bytes.NewReader([]byte("*3\r\n$4\r\nECHO\r\n$5\r\nHello\r\n$5\r\nWorld\r\n")))
+
+	got, err := handleResp(reader)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if got != "Hello World" {
+		t.Errorf("Message = %q, Want = %q", got, "Hello World")
+	}
+
+}
