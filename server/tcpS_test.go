768e0415b9e3520a100016c17c2cd9fbd1cdfa18
Rodrigo Blancas <rodrigoblancas19@gmail.com>
2026-07-27 20:51:07 -0600

    test: add happy path for Set command

--- server/tcpS_test.go
+++ server/tcpS_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"bytes"
+	"sync"
 	"testing"
 
 	"bufio"
@@ -11,12 +12,19 @@ func TestHandleRespInput(t *testing.T) {
 
 	reader := bufio.NewReader(bytes.NewReader([]byte("*1\r\n$4\r\nPING\r\n")))
 
-	got, err := handleResp(reader)
+	s := Server{
+		store: &Memory{
+			mu:    sync.RWMutex{},
+			KVMap: make(map[string]string),
+		},
+	}
+
+	got, err := s.handleResp(reader)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 
-	if got != "PONG" {
+	if got != "+PONG" {
 		t.Errorf("Message = %q, Want = %q", got, "PONG")
 	}
 
@@ -25,13 +33,40 @@ func TestHandleRespInput(t *testing.T) {
 func TestHandleRespEcho(t *testing.T) {
 	reader := bufio.NewReader(bytes.NewReader([]byte("*3\r\n$4\r\nECHO\r\n$5\r\nHello\r\n$5\r\nWorld\r\n")))
 
-	got, err := handleResp(reader)
+	s := Server{
+		store: &Memory{
+			mu:    sync.RWMutex{},
+			KVMap: make(map[string]string),
+		},
+	}
+
+	got, err := s.handleResp(reader)
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
 
-	if got != "Hello World" {
+	if got != "+Hello World" {
 		t.Errorf("Message = %q, Want = %q", got, "Hello World")
 	}
 
+}
+
+func TestHandleRespSet(t *testing.T) {
+	reader := bufio.NewReader(bytes.NewReader([]byte("*3\r\n$3\r\nSET\r\n$4\r\nName\r\n$4\r\nJohn\r\n")))
+
+	s := Server{
+		store: &Memory{
+			mu:    sync.RWMutex{},
+			KVMap: make(map[string]string),
+		},
+	}
+
+	got, err := s.handleResp(reader)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	if got != "+OK" {
+		t.Errorf("Response = %q, Want = %q", got, "OK")
+	}
 }
